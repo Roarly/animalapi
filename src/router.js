e7c22813c945e2cfc0b20285f6f1c37eff13198c
Abel Quintanilla <abelqui15@gmail.com>
2022-03-09 13:50:23 -0600

    Further Changes for API Assgnmt

--- src/router.js
+++ src/router.js
@@ -1,9 +1,6 @@
 import {createRouter, createWebHistory} from "vue-router";
 
 import Index from "./pages/index.vue";
-import About from "./pages/about.vue";
-import Hello from "./pages/hello.vue";
-import Order from "./pages/order.vue";
 import NotFound from "./pages/404.vue";
 
 const routes = [
@@ -11,21 +8,6 @@ const routes = [
     path: "/",
     name: "Index",
     component: Index,
-},
-{
-    path: "/about",
-    name: "About",
-    component: About,
-},
-{
-    path: "/hello",
-    name: "Hello",
-    component: Hello,
-},
-{
-    path: "/order",
-    name: "Order",
-    component: Order,
 },
 {
     path: "/:pathMatch(.*)*",
