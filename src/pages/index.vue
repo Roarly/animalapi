6f64490518a649f9898396aac4ff05a02ddca894
Abel Quintanilla <abelqui15@gmail.com>
2022-03-09 13:55:53 -0600

    Layout Done

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-<div class="container">
-
+<div>
+    <input type="text" placeholder="Search an Axolotl" class="w-full py-4 mt-16 text-xl text-center rounded-full">
 </div>
+<div class="grid grid-cols-2 gap-16 pt-16"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, nobis. Expedita, consequuntur repellendus. Voluptates similique velit placeat quod consectetur tempore ex quo, corporis possimus rem, quos sunt hic perferendis nisi!</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, nobis. Expedita, consequuntur repellendus. Voluptates similique velit placeat quod consectetur tempore ex quo, corporis possimus rem, quos sunt hic perferendis nisi!</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, nobis. Expedita, consequuntur repellendus. Voluptates similique velit placeat quod consectetur tempore ex quo, corporis possimus rem, quos sunt hic perferendis nisi!</p></div>
+
 </template>
